27e297be942f9e4cb76d50e0fab8fbc827bb0247
Banuka Ambegoda <banukaambegoda@gmail.com>
2023-03-16 18:35:45 -0400

    merge

---
+++ project.sql
@@ -0,0 +1,17 @@
+--Name : Banuka Kumara Ambegoda
+--PROG 1870 Project
+--Date : 2022-08-10
+
+USE master;
+
+GO
+
+DROP DATABASE IF EXISTS project;
+
+CREATE DATABASE project;
+
+GO
+USE project;
+
+GO
+
